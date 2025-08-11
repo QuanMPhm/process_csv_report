173dc5cdbf45558088153db197611fd8dcf8b276
Kelvin Lin <kelvin@bu.edu>
2025-08-11 18:12:17 -0400

    Switch print statements to logger
    Closes https://github.com/CCI-MOC/invoicing/issues/223. Replace print statements in invoicing repo with logger for logger functionatlity.

--- process_report/institute_list_validate.py
+++ process_report/institute_list_validate.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import pydantic
 import yaml
+import logging
 
 from process_report.institute_list_models import InstituteList
 
@@ -36,7 +37,7 @@ def main(arg_list: list[str] | None = None):
         with open(args.institute_list) as f:
             data = yaml.safe_load(f)
             institute_list = InstituteList.model_validate(data)
-            print(
+            logging.info(
                 f"Validation of {len(institute_list.root)} institution entries successful"
             )
     except pydantic.ValidationError as err:
